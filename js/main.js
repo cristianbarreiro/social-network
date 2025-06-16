549efde8ef732ba8040318fdf78f288a34999d81
Cristian <cb2onix@gmail.com>
2025-06-16 00:09:18 -0300

    =feat: about us

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ document.addEventListener("DOMContentLoaded", function () {
             <li><a href="../index.html"><i class="fa fa-home nav-icon"></i><span>HOME</span></a></li>
             <li><a href="../about.html"><i class="fa fa-info nav-icon"></i><span>About Us</span></a></li>
             <li><a href="../prices.html"><i class="fa fa-usd nav-icon"></i><span>Premium Prices</span></a></li>
-            <li><a href="../faqs.html"><i class="fa fa-circle-question nav-icon"></i><span>FAQs</span></a></li>
             <li><a href="../support.html"><i class="fa fa-headset nav-icon"></i><span>Support</span></a></li>
             <li><a href="../config.html"><i class="fa fa-gear nav-icon"></i><span class="text-label">Configuration</span></a></li>
           </ul>
@@ -36,4 +35,20 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   `;
   document.head.appendChild(style);
+});
+
+const faqsTitles = document.querySelectorAll(".faq .head");
+
+faqsTitles.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.parentNode.classList.toggle("active");
+
+    const faqContent = faq.nextElementSibling;
+
+    if (faq.parentNode.classList.contains("active")) {
+      faqContent.style.height = faqContent.scrollHeight + 30 + "px";
+    } else {
+      faqContent.style.height = "0px";
+    }
+  });
 });
