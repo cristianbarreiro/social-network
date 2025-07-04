f137700664a419ff82dcfc02ea696b27eeebadde
Cristian <122134947+cristianbarreiro@users.noreply.github.com>
2025-07-03 21:26:57 -0300

    feat: tawk.to assistant

--- scripts/main.js
+++ scripts/main.js
@@ -51,6 +51,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
   navbarPlaceholder.innerHTML = navbarHTML;
 
+  // Carga del script de Tawk.to según el snippet oficial
+  (function () {
+    var Tawk_API = Tawk_API || {},
+      Tawk_LoadStart = new Date();
+    var s1 = document.createElement("script"),
+      s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = "https://embed.tawk.to/686719d6dd67f9190a43345c/1iv9bprle";
+    s1.charset = "UTF-8";
+    s1.setAttribute("crossorigin", "*");
+    s0.parentNode.insertBefore(s1, s0);
+  })();
+
   const supportLink = document.getElementById("support-link");
   const modal = document.getElementById("support-modal");
   const closeModal = modal.querySelector(".close-modal");
